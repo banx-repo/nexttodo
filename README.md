0aea68a1cac9d746bae5dbfd4970e5c2c2b26147
Béo Ú <3nx9nd@gmail.com>
2020-09-30 17:11:31 +0700

    Update

--- README.md
+++ README.md
@@ -1,30 +1,40 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Hướng dẫn sử dụng trước khi dùng
 
-## Getting Started
+1. MySQL
 
-First, run the development server:
+`docker run --name nexttodo -e MYSQL_ROOT_PASSWORD=bnx -p 3306:3306 -d mysql:5`
 
-```bash
-npm run dev
-# or
-yarn dev
+2. Tạo cơ sở dữ liệu
+
+```sql
+CREATE DATABASE IF NOT EXISTS todolist
+CHARACTER SET utf8;
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+3. Tạo bảng
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+```sql
+CREATE TABLE IF NOT EXISTS user (
+id INT auto_increment PRIMARY KEY,
+fullname VARCHAR(255) NOT NULL,
+username VARCHAR(10) NOT NULL,
+password VARCHAR(255) NOT NULL,
+avatar TEXT,
+created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+```
 
-## Learn More
+```sql
+CREATE TABLE IF NOT EXISTS todo (
+id INT AUTO_INCREMENT PRIMARY KEY,
+content TEXT NOT NULL,
+created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+completed TINYINT(1) DEFAULT 0,
+user_id INT,
+FOREIGN KEY (user_id) REFERENCES user (id));
+```
 
-To learn more about Next.js, take a look at the following resources:
+4. Run
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+`npm run dev`
--- pages/api/todo.js
+++ pages/api/todo.js
@@ -2,84 +2,91 @@ import query from "../../lib/mysql";
 import jwt from "jsonwebtoken";
 
 export default async (req, res) => {
-  const token = req.cookies.token;
-  const tokenData = jwt.verify(token, process.env.SECRET_KEY);
+    const token = req.cookies.token;
+    const tokenData = jwt.verify(token, process.env.SECRET_KEY);
 
-  if (!tokenData) {
-    res.status(403).json({ message: "Cút 😵!" });
-    return;
-  }
+    if (!tokenData) {
+        res.status(403).json({ message: "Cút 😵!" });
+        return;
+    }
 
-  switch (req.method) {
-    case "GET":
-      var result = await query(
-        "SELECT id, content, modified, completed FROM todo WHERE user_id = ? ORDER BY modified DESC",
-        tokenData.id
-      );
+    switch (req.method) {
+        case "GET":
+            var result = await query(
+                "SELECT id, content, modified, completed FROM todo WHERE user_id = ? ORDER BY modified DESC",
+                tokenData.id
+            );
 
-      if (!result) {
-        res.status(400).json({ message: "Very veryyyy bad request! 😵" });
-        break;
-      }
+            if (!result) {
+                res.status(400).json({
+                    message: "Very veryyyy bad request! 😵",
+                });
+                break;
+            }
 
-      res.status(200).json(result);
-      break;
+            res.status(200).json(result);
+            break;
 
-    case "POST":
-      var content = req.body.todo;
-      if (!content || content.trim().length === 0) {
-        res.status(400).json({ message: "Very veryyyy bad request! 😵" });
-        break;
-      }
+        case "POST":
+            var content = req.body.todo.replace(/[\u0800-\uFFFF]/g, "");
+            if (!content || content.trim().length === 0) {
+                res.status(400).json({
+                    message: "Very veryyyy bad request! 😵",
+                });
+                break;
+            }
 
-      var result = await query(
-        "INSERT INTO todo (content, user_id) VALUE(?, ?)",
-        [content, tokenData.id]
-      );
+            var result = await query(
+                "INSERT INTO todo (content, user_id) VALUE(?, ?)",
+                [content, tokenData.id]
+            );
 
-      if (!result) {
-        res.status(400).json({ message: "Lỗi tùm lum 😵!" });
-        break;
-      }
+            if (!result) {
+                res.status(400).json({ message: "Lỗi tùm lum 😵!" });
+                break;
+            }
 
-      res.status(200).json({ message: "Thêm Ô kê 😘!" });
-      break;
+            res.status(200).json({ message: "Thêm Ô kê 😘!" });
+            break;
 
-    case "PUT":
-      var { id, content, completed } = req.body;
-      var modified = new Date().toISOString().slice(0, 19).replace("T", " ");
+        case "PUT":
+            var { id, content, completed } = req.body;
+            var modified = new Date()
+                .toISOString()
+                .slice(0, 19)
+                .replace("T", " ");
 
-      var result = await query(
-        "UPDATE todo SET content = ?, modified = ?, completed = ? WHERE id = ? AND user_id = ?",
-        [content, modified, completed, id, tokenData.id]
-      );
+            var result = await query(
+                "UPDATE todo SET content = ?, modified = ?, completed = ? WHERE id = ? AND user_id = ?",
+                [content, modified, completed, id, tokenData.id]
+            );
 
-      if (!result) {
-        res.status(400).json({ message: "Lỗi tùm lum 😵!" });
-        break;
-      }
+            if (!result) {
+                res.status(400).json({ message: "Lỗi tùm lum 😵!" });
+                break;
+            }
 
-      res.status(200).json({ message: "Sửa Ô kê 🤓!" });
-      break;
+            res.status(200).json({ message: "Sửa Ô kê 🤓!" });
+            break;
 
-    case "DELETE":
-      var { id } = req.body;
+        case "DELETE":
+            var { id } = req.body;
 
-      var result = await query(
-        "DELETE FROM todo WHERE id = ? AND user_id = ?",
-        [id, tokenData.id]
-      );
+            var result = await query(
+                "DELETE FROM todo WHERE id = ? AND user_id = ?",
+                [id, tokenData.id]
+            );
 
-      if (!result) {
-        res.status(400).json({ message: "Lỗi tùm lum 😵!" });
-        break;
-      }
+            if (!result) {
+                res.status(400).json({ message: "Lỗi tùm lum 😵!" });
+                break;
+            }
 
-      res.status(200).json({ message: "Xóa Ô kê 😭!" });
-      break;
+            res.status(200).json({ message: "Xóa Ô kê 😭!" });
+            break;
 
-    default:
-      res.status(400).json({ message: "Method not allowed 😵!" });
-      break;
-  }
+        default:
+            res.status(400).json({ message: "Method not allowed 😵!" });
+            break;
+    }
 };
